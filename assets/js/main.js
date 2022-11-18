0d2087dd7d35137895a4926c3998daeee211eee9
brucemorakane <brucemorakane74@gmail.com.com>
2022-11-18 15:35:03 +0200

    conatct form added

--- assets/js/main.js
+++ assets/js/main.js
@@ -209,3 +209,18 @@
   new PureCounter();
 
 })()
+
+function sendMail(str) {
+  var link = "mailto:mbuyisolo@gmail.com" + "&subject=" + escape("This is my subject") + "&body=" + escape(str);
+  location.href = link;
+}
+function submit_comment() {
+var name = document.forms["contact_form"]["Name"].value;
+var Email = document.forms["contact_form"]["Email"].value;
+var Phone = document.forms["contact_form"]["Phone"].value;
+var Location = document.forms["contact_form"]["Location"].value;
+var Comment = document.forms["contact_form"]["Location"].value;
+if((name !="") && (Email !="") && (Phone !="") && (Location !="") && (Comment !="")) {
+ sendMail("HELLO");
+}
+}
